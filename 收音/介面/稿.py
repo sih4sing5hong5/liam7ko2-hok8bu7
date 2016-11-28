2c5dcb3c0a532546aa3e79407193a71e02b86ed3
薛丞宏 <ihcaoe@gmail.com>
2016-11-28 12:07:50 +0800

    有法度組音檔出來矣

--- 收音/介面/稿.py
+++ 收音/介面/稿.py
@@ -1,9 +1,14 @@
 
+from base64 import b64decode
+import json
+
 from django.core.files.base import ContentFile
 from django.db.models.aggregates import Max
 from django.forms.models import model_to_dict
 from django.http.response import JsonResponse
 from django.views import View
+
+
 from 收音.models import 例句音檔表
 from 收音.models import 例句表
 
@@ -20,10 +25,13 @@ class 稿(View):
             啥人唸的=啥人唸的,
             例句=例句表.objects.get(pk=編號),
         )
+        資料陣列=bytes(json.loads('['+b64decode(request.POST['blob']).decode('utf-8')+']'))
         例句音檔.音檔.save(
             '錄音檔-{}-{}.wav'.format(啥人唸的, 編號),
-            ContentFile(request.POST['blob'])
+            ContentFile(資料陣列)
         )
+        print(資料陣列[:200])
+        
         return self.揣後一筆(request.POST['啥人唸的'])
 
     def 揣後一筆(self, 啥人唸的):
