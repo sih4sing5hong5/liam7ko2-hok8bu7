cf4881dd4a6736ae3cd2a715be3236a67351e140
薛丞宏 <ihcaoe@gmail.com>
2016-11-28 10:28:18 +0800

    改用max fix #1

--- 收音/介面/稿.py
+++ 收音/介面/稿.py
@@ -1,4 +1,5 @@
 
+from django.db.models.aggregates import Max
 from django.forms.models import model_to_dict
 from django.http.response import JsonResponse
 from django.views import View
@@ -20,6 +21,9 @@ class 稿(View):
         return self.揣後一筆(request.POST['啥人唸的'])
 
     def 揣後一筆(self, 啥人唸的):
-        數量 = 例句音檔表.objects.filter(啥人唸的=啥人唸的).count()
-        例句 = 例句表.objects.get(pk=數量 + 1)
+        數量 = 例句音檔表.objects.filter(啥人唸的=啥人唸的).aggregate(上尾一句=Max('pk'))['上尾一句']
+        try:
+            例句 = 例句表.objects.get(pk=數量 + 1)
+        except:
+            例句 = 例句表.objects.get(pk=1)
         return JsonResponse(model_to_dict(例句))
