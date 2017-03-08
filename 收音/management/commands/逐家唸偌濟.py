6192d1bfae4079273e5848231a175e556e84f111
薛丞宏 <ihcaoe@gmail.com>
2017-03-08 10:49:31 +0800

    除錯，而且免顯示資料數量

--- 收音/management/commands/逐家唸偌濟.py
+++ 收音/management/commands/逐家唸偌濟.py
@@ -1,6 +1,5 @@
 import json
 
-from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 
@@ -10,11 +9,10 @@ from 收音.models import 例句音檔表
 class Command(BaseCommand):
 
     def handle(self, *args, **參數):
-        call_command('顯示資料數量')
         全部 = {}
-        for 例句音檔 in 例句音檔表.objects.values_list('啥人唸的', flat=True):
+        for 啥人唸的 in 例句音檔表.objects.values_list('啥人唸的', flat=True):
             try:
-                全部[例句音檔.啥人唸的] += 1
+                全部[啥人唸的] += 1
             except:
-                全部[例句音檔.啥人唸的] = 1
+                全部[啥人唸的] = 1
         print(json.dumps(全部, indent=2, ensure_ascii=False))
